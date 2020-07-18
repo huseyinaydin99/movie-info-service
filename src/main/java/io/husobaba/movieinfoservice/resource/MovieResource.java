20f99e35c0e739d68d5387479f35d081bcd0fa47
huso51 <huseyinaydin99@gmail.com>
2020-07-18 09:00:30 +0300

    Movie-info-services. Microservices ile film bilgi servisi yazdık.

---
+++ src/main/java/io/husobaba/movieinfoservice/resource/MovieResource.java
@@ -0,0 +1,16 @@
+package io.husobaba.movieinfoservice.resource;
+
+import io.husobaba.movieinfoservice.model.Movie;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movie")
+public class MovieResource {
+
+    @RequestMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable("movieId") String movieId){
+        return new Movie(movieId,"Test Name");
+    }
+}
